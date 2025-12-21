e60aebccbf0e092ab38ffcc584fa45a55de32d4b
Siddharth-prog <siddharth.20235147@mnnit.ac.in>
2025-12-21 12:28:04 +0530

    Initial commit

---
+++ forntend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-red-600 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+        Tailwind LIVE
+      </h1>
+    </div>
+  );
+}
